1cd0a633549532ac0ad9a0d613e89a4f81a567ca
HuproZ12 <hugodtlx@outlook.com>
2024-11-08 16:07:38 +0100

    maj

--- reporting/script.js
+++ reporting/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const calendar = document.getElementById('calendar');
     const taskWidget = document.getElementById('task-widget');
+    const taskContent = document.getElementById('task-content');
+    const selectedDateElement = document.getElementById('selected-date');
     const backButton = document.getElementById('back-button');
 
     const daysOfWeek = ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'];
@@ -52,6 +54,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 ## Après-midi : 13h30-17h
 - Apprentissage Blender/GIMP, avec pour objectif de se familiariser avec la modélisation/manipulation d'image.Pour l'instant dans un objectif de modding Minecraft`,
+            '6': `## Matin : 9h-12h30
+- [Recherche](https://1drv.ms/x/c/b2c781cd25a62a9c/Ef6sVK0IPWFJnIgmviU7zJ0BFBIcSfTW-QedKNZyXlSzCg?e=Ihho6E) d'emploi
+
+## Après-midi : 13h30-17h
+- [Recherche](https://1drv.ms/x/c/b2c781cd25a62a9c/Ef6sVK0IPWFJnIgmviU7zJ0BFBIcSfTW-QedKNZyXlSzCg?e=Ihho6E) d'emploi`,
+            '7': `## Matin : 9h-12h30
+- Reflexion sur la forme du reporting de stage
+
+## Après-midi : 13h30-17h
+- Commencement de la construction de cette GitHub Page, qui servira de portfolio/reporting pour la période de stage`,
+            '8': `## Matin : 9h-12h30
+- Peaufinement de la GitHub Page :
+    - index
+    - navigation, connexions
+    - reporting :
+        - calendrier
+        - historique des tâches
+
+## Après-midi : 13h30-17h
+- [Recherche](https://1drv.ms/x/c/b2c781cd25a62a9c/Ef6sVK0IPWFJnIgmviU7zJ0BFBIcSfTW-QedKNZyXlSzCg?e=Ihho6E) d'emploi`,
         };
 
         const taskMarkdown = tasks[date] || `## Aucune tâche`;
@@ -60,7 +82,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const taskHTML = marked(taskMarkdown);
 
         // Mise à jour du widget avec le HTML généré
-        taskWidget.innerHTML = taskHTML;
+        taskContent.innerHTML = taskHTML;
+        selectedDateElement.textContent = `${date} novembre 2024`;
         taskWidget.classList.add('fade-in');
         setTimeout(() => {
             taskWidget.classList.remove('fade-in');
